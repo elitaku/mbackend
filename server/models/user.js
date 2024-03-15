af3848cdc0f2a2f50bac59de97c0af6dd25d6465
JD <jaysondorilag1@gmail.com>
2024-03-15 22:10:39 +0800

    Connecting To Atlas Cloud &  Creating User Schema

--- server/models/user.js
+++ server/models/user.js
@@ -4,50 +4,50 @@ import mongoose from "mongoose";
 // import jwt from "jsonwebtoken";
 
 const schema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, "Please Enter Name"],
-//   },
+  name: {
+    type: String,
+    required: [true, "Please Enter Name"],
+  },
 
-//   email: {
-//     type: String,
-//     required: [true, "Please Enter Email"],
-//     unique: [true, "Email Already Exist"],
-//     validate: validator.isEmail,
-//   },
-//   password: {
-//     type: String,
-//     required: [true, "Please Enter Password"],
-//     minLength: [6, "Password must be at least 6 characters long"],
-//     select: false,
-//   },
-//   address: {
-//     type: String,
-//     required: true,
-//   },
-//   city: {
-//     type: String,
-//     required: true,
-//   },
-//   country: {
-//     type: String,
-//     required: true,
-//   },
-//   pinCode: {
-//     type: Number,
-//     required: true,
-//   },
-//   role: {
-//     type: String,
-//     enum: ["admin", "user"],
-//     default: "user",
-//   },
-//   avatar: {
-//     public_id: String,
-//     url: String,
-//   },
-//   otp: Number,
-//   otp_expire: Date,
+  email: {
+    type: String,
+    required: [true, "Please Enter Email"],
+    unique: [true, "Email Already Exist"],
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Password"],
+    minLength: [6, "Password must be at least 6 characters long"],
+    select: false,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  pinCode: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  otp: Number,
+  otp_expire: Date,
 });
 
 // schema.pre("save", async function (next) {
