a81fb252774d437051045eecb88c417703819140
JD <jaysondorilag1@gmail.com>
2024-03-15 21:33:15 +0800

    Section 11: Backend Setup 50. Setting Up Backend

---
+++ server/app.js
@@ -0,0 +1,10 @@
+import express  from "express";
+import { config } from "dotenv";
+
+
+config({
+    path: "./data/config.env",
+  });
+  
+export const app = express();
+
---
+++ server/server.js
@@ -0,0 +1,8 @@
+import { app } from "./app.js";
+
+
+app.listen(process.env.PORT, () => {
+    console.log(
+      `Server listening on port: ${process.env.PORT}, in ${process.env.NODE_ENV} MODE.`
+    );
+  });
