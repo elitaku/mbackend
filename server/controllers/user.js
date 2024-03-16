d27833e7c0c066878a62e328451d40d750a3149f
notshawarma <ejcezar.falogme@gmail.com>
2024-03-16 10:34:50 +0800

    Section 15: Update Profile Routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,10 +3,10 @@ import ErrorHandler from "../utils/error.js";
 import { asyncError } from "../middlewares/error.js";
 import { cookieOptions, sendToken } from "../utils/features.js";
 
-export const login = async(req, res, next) => {
+export const login = async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email }).select("+password");
-  
+
   if (!user) {
     return next(new ErrorHandler("Incorrect Email or Password", 400));
   }
@@ -19,7 +19,7 @@ export const login = async(req, res, next) => {
   if (!isMatched) {
     return next(new ErrorHandler("Incorrect Password"));
   }
-  
+
   sendToken(user, res, `Welcome Back, ${user.name}`, 200);
 };
 
@@ -63,4 +63,48 @@ export const logOut = asyncError(async (req, res, next) => {
       success: true,
       message: "Logged Out Successfully",
     });
+});
+
+export const updateProfile = asyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  const { name, email, address, city, country, pinCode } = req.body;
+
+  if (name) user.name = name;
+  if (email) user.email = email;
+  if (address) user.address = address;
+  if (city) user.city = city;
+  if (country) user.country = country;
+  if (pinCode) user.pinCode = pinCode;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Updated Successfully",
+  });
+});
+
+
+export const changePassword = asyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id).select("+password");
+
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword)
+    return next(
+      new ErrorHandler("Please Enter Old Password & New Password", 400)
+    );
+
+  const isMatched = await user.comparePassword(oldPassword);
+
+  if (!isMatched) return next(new ErrorHandler("Incorrect Old Password", 400));
+
+  user.password = newPassword;
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Password Changed Successully",
+  });
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMyProfile, login, logOut, signup } from "../controllers/user.js";
+import { changePassword, getMyProfile, login, logOut, signup, updateProfile } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.post("/login", login);
 router.post("/new", signup);
 router.get("/me", isAuthenticated, getMyProfile);
 router.get("/logout", isAuthenticated, logOut);
+
+router.put("/updateprofile", isAuthenticated, updateProfile)
+router.put("/changepassword", isAuthenticated, changePassword)
 
 export default router;
