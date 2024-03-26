667a6e16a5ef314224f926bd5fa1aeaf3bd80e78
JD <jaysondorilag1@gmail.com>
2024-03-27 00:00:32 +0800

    comment backend changes

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -8,8 +8,8 @@ export const getAllComments = asyncError(async (req, res, next) => {
   try {
     const productId = req.params.productId; // Assuming product ID is passed as a parameter
 
-    // Find all comments related to the specified product ID and populate the 'user' field with the 'name' of the user
-    const comments = await Comment.find({ product: productId }).populate('user', 'name');
+    // Find all comments related to the specified product ID
+    const comments = await Comment.find({ product: productId });
 
     res.json(comments);
   } catch (error) {
@@ -17,7 +17,6 @@ export const getAllComments = asyncError(async (req, res, next) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 
 
 // Add a new comment
@@ -133,26 +132,3 @@ export const deleteComment = asyncError(async (req, res, next) => {
   }
 });
 
-
-export const getProductRatings = asyncError(async (req, res, next) => {
-  try {
-    const productId = req.params.productId; 
-
-    const comments = await Comment.find({ product: productId });
-
-    if (comments.length === 0) {
-      return res.status(404).json({ success: false, message: "No ratings found for this product" });
-    }
-
-    let totalRating = 0;
-    comments.forEach(comment => {
-      totalRating += comment.rating;
-    });
-    const averageRating = totalRating / comments.length;
-
-    res.status(200).json({ success: true, averageRating });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
-});
